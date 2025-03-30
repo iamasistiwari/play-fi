645b60cf56fd7768d5596205b83856c66af7aab1
iamasistiwari <iamashishtiwari2004@gmail.com>
2025-03-30 22:53:04 +0530

    completing-room

--- apps/ws/src/helpers/Room.ts
+++ apps/ws/src/helpers/Room.ts
@@ -1,26 +1,32 @@
+//@ts-ignore
+import ytSearch from "youtube-search-api";
 import { WebSocket } from "ws";
-import axios from "axios";
-import { FromWebSocketMessages, Song } from "@repo/common/type";
+import {
+  FromWebSocketMessages,
+  YoutubeVideoDetails,
+  StoreSongs,
+  VoteSong,
+} from "@repo/common/type";
 import { AxiosError } from "axios";
 
 export default class Room {
   private joinedPlayers: Map<string, WebSocket>;
   private roomTitle: string;
   private roomPassword: string;
-  private accessToken: string;
   private adminName: string;
   private roomId: string;
   private userLimitSong: Map<string, { count: number; timeStamp: number[] }>;
   private adminId: string;
-  private currentSongQueue: [];
+  private currentSongQueue: StoreSongs[];
   private TIME_WINDOW = 20 * 60 * 1000;
   private SONG_LIMIT = 3;
+  // key must be like `socket.userId:songId`
+  private votingDetails: Map<string, boolean>;
 
   constructor(
     socket: WebSocket,
     roomTitle: string,
     roomPassword: string,
-    accessToken: string,
     roomId: string
   ) {
     this.currentSongQueue = [];
@@ -28,59 +34,26 @@ export default class Room {
     this.roomPassword = roomPassword;
     this.roomTitle = roomTitle;
     this.adminId = socket.userId;
-    this.accessToken = accessToken;
     this.joinedPlayers.set(socket.userId, socket);
     this.adminName = socket.userName;
     this.roomId = roomId;
     this.userLimitSong = new Map();
+    this.votingDetails = new Map();
   }
+
   addPersons(socket: WebSocket) {
-    const isPresent = this.joinedPlayers.get(socket.userId);
-    if (!isPresent) {
-      this.joinedPlayers.set(socket.userId, socket);
-      const sendMsg: FromWebSocketMessages = {
-        type: "joined",
-        message: `${this.roomTitle}`,
-        metadata: {
-          room_id: this.roomId,
-          room_title: this.roomTitle,
-          joined_persons: this.joinedPlayers.size,
-          owner_name: this.adminName,
-        },
-      };
-      socket.send(JSON.stringify(sendMsg));
-      this.joinedPlayers.forEach((socket) => {
-        const sendMsg: FromWebSocketMessages = {
-          type: "metadata",
-          metadata: {
-            room_id: this.roomId,
-            room_title: this.roomTitle,
-            joined_persons: this.joinedPlayers.size,
-            owner_name: this.adminName,
-          },
-        };
-        socket.send(JSON.stringify(sendMsg));
-      });
-      return;
-    }
+    this.joinedPlayers.set(socket.userId, socket);
     const sendMsg: FromWebSocketMessages = {
-      type: "close",
-      message: `close the other tab`,
+      type: "joined",
+      message: `${this.roomTitle}`,
+      metadata: {
+        room_id: this.roomId,
+        room_title: this.roomTitle,
+        joined_persons: this.joinedPlayers.size,
+        owner_name: this.adminName,
+      },
     };
-    return socket.send(JSON.stringify(sendMsg));
-  }
-  getRoomPassword() {
-    return this.roomPassword;
-  }
-  checkPersonPresence(socket: WebSocket) {
-    const isPresent = this.joinedPlayers.get(socket.userId);
-    if (!isPresent) {
-      return false;
-    }
-    return true;
-  }
-  handleClose(socket: WebSocket) {
-    this.joinedPlayers.delete(socket.userId);
+    socket.send(JSON.stringify(sendMsg));
     this.joinedPlayers.forEach((socket) => {
       const sendMsg: FromWebSocketMessages = {
         type: "metadata",
@@ -94,19 +67,22 @@ export default class Room {
       socket.send(JSON.stringify(sendMsg));
     });
   }
+  getRoomPassword() {
+    return this.roomPassword;
+  }
+  checkPersonPresence(socket: WebSocket) {
+    const isPresent = this.joinedPlayers.get(socket.userId);
+    if (!isPresent) {
+      return false;
+    }
+    return true;
+  }
   async searchSong(socket: WebSocket, song: string) {
     try {
-      const songResult = await axios.get(
-        `https://api.spotify.com/v1/search?q=${song}&type=track`,
-        {
-          headers: {
-            Authorization: `Bearer ${this.accessToken}`,
-          },
-        }
-      );
+      const results = await ytSearch.GetListByKeyword(`${song}`, false);
       const sendMsg: FromWebSocketMessages = {
         type: "songs",
-        message: JSON.stringify(songResult.data),
+        message: JSON.stringify(results),
       };
       return socket.send(JSON.stringify(sendMsg));
     } catch (error) {
@@ -120,10 +96,41 @@ export default class Room {
     }
   }
 
-  handleVote() {}
+  addSongToQueue(socket: WebSocket, song: YoutubeVideoDetails) {
+    //content should be valid video
+    if (song.type !== "video") {
+      const sendMsg: FromWebSocketMessages = {
+        type: "error",
+        message: "Invalid video properties",
+      };
+      return socket.send(JSON.stringify(sendMsg));
+    }
 
-  addSongToQueue(socket: WebSocket, userSendedSong: Song) {
-    //check users maxAdded song timestamp
+    //check current queue length
+    if (this.currentSongQueue.length >= 20) {
+      const sendMsg: FromWebSocketMessages = {
+        type: "error",
+        message: "Queue is full",
+      };
+      return socket.send(JSON.stringify(sendMsg));
+    }
+
+    // now check is video length is not greater than 10 min && less than 1
+    const songMinutes = Number(song.length.simpleText.split(":")[0]);
+    if (songMinutes > 10) {
+      const sendMsg: FromWebSocketMessages = {
+        type: "error",
+        message: "Cannot add song over 10 minutes",
+      };
+      return socket.send(JSON.stringify(sendMsg));
+    }
+    if (songMinutes < 1) {
+      const sendMsg: FromWebSocketMessages = {
+        type: "error",
+        message: "Cannot add song less than 1 minute",
+      };
+      return socket.send(JSON.stringify(sendMsg));
+    }
 
     const current_time = Date.now();
 
@@ -132,33 +139,123 @@ export default class Room {
     }
 
     let userSongs = this.userLimitSong.get(socket.userId);
-
     // remove expired timestamp
     userSongs!.timeStamp = userSongs!.timeStamp.filter(
       (timeStamp) => current_time - timeStamp < this.TIME_WINDOW
     );
-
     userSongs!.count = userSongs!.timeStamp.length;
-
     if (userSongs!.count >= this.SONG_LIMIT) {
       const sendMsg: FromWebSocketMessages = {
         type: "error",
-        message: "You can only add up to 3 songs in 20 minutes.",
+        message: "You can only add up to 3 songs in 20 minutes",
+      };
+      return socket.send(JSON.stringify(sendMsg));
+    }
+
+    //check if songs exits or not
+    const isAdded = this.currentSongQueue.some((addedSong) => {
+      return addedSong.id === song.id;
+    });
+    if (isAdded) {
+      const sendMsg: FromWebSocketMessages = {
+        type: "error",
+        message: "Can't add same song again",
       };
       return socket.send(JSON.stringify(sendMsg));
     }
 
     // add song to queee
+    const songToAdd = {
+      ...song,
+      votes: 0,
+      addedBy: socket.userName,
+      voted: false,
+    };
+    this.currentSongQueue.push(songToAdd);
 
+    //sort the array des
+    this.currentSongQueue.sort((a, b) => b.votes - a.votes);
 
-    // update all user values
+    // update all user values & send success
+    userSongs!.timeStamp.push(current_time);
+    userSongs!.count++;
 
-    userSongs!.timeStamp.push(current_time)
-    userSongs!.count++
-    const sendMsg:FromWebSocketMessages = {
+    //send updated queue to all
+    this.broadCastQueue();
+
+    const sendMsg: FromWebSocketMessages = {
       type: "success",
-      message: `added to the queue.`,
+      message: `song added`,
     };
     return socket.send(JSON.stringify(sendMsg));
+  }
+
+  voteSong(socket: WebSocket, songId: string) {
+    //check whether song is in the
+    const isSongInQueue = this.currentSongQueue.some((addedSong) => {
+      return addedSong.id === songId;
+    });
+
+    if (!isSongInQueue) {
+      const sendMsg: FromWebSocketMessages = {
+        type: "error",
+        message: "Song is not in the queue",
+      };
+      return socket.send(JSON.stringify(sendMsg));
+    }
+    // is user voted that song if yes reverse it and if not vote it
+    const isVoted = this.votingDetails.get(`${socket.userId}:${songId}`);
+    if (!isVoted) {
+      this.votingDetails.set(`${socket.userId}:${songId}`, true);
+      //now increase song vote also
+      this.currentSongQueue = this.currentSongQueue.map((val) => {
+        if (val.id === songId) {
+          return {
+            ...val,
+            votes: val.votes + 1,
+          };
+        }
+        return {
+          ...val,
+        };
+      });
+      this.broadCastQueue();
+    } else {
+      this.votingDetails.set(`${socket.userId}:${songId}`, false);
+
+      //now decrease the song vote also
+      this.currentSongQueue = this.currentSongQueue.map((val) => {
+        if (val.id === songId) {
+          return {
+            ...val,
+            votes: val.votes - 1,
+          };
+        }
+        return {
+          ...val,
+        };
+      });
+      this.broadCastQueue();
+    }
+  }
+
+  private broadCastQueue() {
+    this.joinedPlayers.forEach((socket) => {
+      const userSongArray = this.currentSongQueue.map((song) => {
+        let isVoted = this.votingDetails.get(`${socket.userId}:${song.id}`);
+        if (!isVoted) {
+          isVoted = false;
+        }
+        return {
+          ...song,
+          voted: isVoted,
+        };
+      });
+      const sendMsg: FromWebSocketMessages = {
+        type: "queue",
+        queue: userSongArray,
+      };
+      socket.send(JSON.stringify(sendMsg));
+    });
   }
 }
